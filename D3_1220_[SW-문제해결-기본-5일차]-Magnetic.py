a20b70717c3dfb33f7739084600269e48c0f0fa3
tesschung <geobera0910@naver.com>
2019-07-23 21:35:26 +0900

    D3_1220_Magnetic 	- Add Description For Code Review

--- D3_1220_[SW-문제해결-기본-5일차]-Magnetic.py
+++ D3_1220_[SW-문제해결-기본-5일차]-Magnetic.py
@@ -10,23 +10,29 @@ for T in range(10):
     new_lists = []
     count = 0
 
-    for t in range(n):
+    for t in range(n):                          # board를 설정한다.
         new_lists.append(list(map(int, input().split())))
 
     for i in range(n):
         for j in range(n):
+                                                # 1을 찾으면,
             if new_lists[j][i] == 1:
                 flag = 0
-                for d in range(j+1, n):
 
-                    if new_lists[d][i] == 1:
+                for d in range(j+1, n):         # 2를 찾기 시작하는데,
+                    if new_lists[d][i] == 1:    # 1인 경우 빠져나와서 새로운 d를 받고,
                         break
-                    elif new_lists[d][i] == 2:
-                        count += 1
+                    elif new_lists[d][i] == 2:  # 2인 경우 (어차피 하나만 찾아도 패턴을 충족하므로)
+                        count += 1              # count를 해주고,
                         flag += 1
                         if flag == 1:
                             break
+                            '''
+                            flag를 설정해서 2를 하나라도 발견하면 바로 나와서 
+                            그 다음의 1부터 다시 시작하여 패턴을 찾도록 했다.
 
+                            flag 설정 중요!
+                            '''
 
     print(f'#{T+1} {count}')
 
