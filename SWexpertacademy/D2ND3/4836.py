cd3b3594a4d7e78c18db70b53b4c2b7410f6692a
tesschung <geobera0910@naver.com>
2019-08-06 08:42:30 +0900

    Update

--- SWexpertacademy/D2ND3/4836.py
+++ SWexpertacademy/D2ND3/4836.py
@@ -1,6 +1,11 @@
 import sys
 sys.stdin = open('4836.txt', 'r')
 
-for tc in range(int(input())):
-    for r in range(int(input())
+T = int(input())
+for tc in range(T):
+    R = int(input())
+    for r in range(R):
+        r_list = list(map(int, input().split()))
+
+
 
