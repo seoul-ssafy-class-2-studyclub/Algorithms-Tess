b73694f1b0b7efec17abad0fca8a7cc3fb0efa43
tesschung <geobera0910@naver.com>
2019-08-27 23:37:43 +0900

    2001 _파리퇴치_2차원배열

---
+++ SWexpertacademy/D2/2001_파리퇴치_2차원배열.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open('2001.txt', 'r')
+
+for tc in range(1, int(input())+1):
+
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+
+
+    counting = []
+    for ny in range(N-M+1):
+        for nx in range(N-M+1):
+            cnt = 0
+            for my in range(M):
+                for mx in range(M):
+                    Y = ny + my
+                    X = nx + mx
+                    if 0 <= Y < N-1 or 0 <= X < N-1:
+                        cnt += board[Y][X]
+            counting.append(cnt)
+    print(f'#{tc}', max(counting))
+
+
+'''
+#1 49
+#2 159
+#3 428
+#4 620
+#5 479
+#6 941
+#7 171
+#8 968
+#9 209
+#10 1242
+'''
