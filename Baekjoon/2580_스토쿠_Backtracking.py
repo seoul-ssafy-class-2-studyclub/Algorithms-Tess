71a0de1cdaebd6816b58117eadc5b4211a437eb7
tesschung <geobera0910@naver.com>
2019-09-05 12:03:54 +0900

    2580_스도쿠_Backtracking

--- Baekjoon/2580_스토쿠_Backtracking.py
+++ Baekjoon/2580_스토쿠_Backtracking.py
@@ -15,61 +15,65 @@
 
 import sys
 sys.stdin = open('2580.txt', 'r')
-sys.setrecursionlimit(10000000)
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+
 # main, inside of itself
 def solve(bo):
     find = find_empty(bo)  # 시작점에서 가능한 곳인지 확인
     if not find:
         return True
-    else:
-        row, col = find
-    for i in range(1, 10):
-        if vaild(bo, i, (row, col)):
-            bo[row][col] = i  # 만약 valid하다고 판단되었다면, 위와 같이 보드에 추가해준다.
+
+    else: # find로 찾은 좌표부터 시작한다.
+        row, col = find # 튜플로 들어온 행과 열에 대한 좌표를 받아서,
+    for i in range(1, 10): # 1부터 10까지 해당 좌표를 기준으로 가로 세로 3*3을 검사한다.
+        if vaild(bo, i, (row, col)): # valid에 변수들이 할당되는데, 보드, 임시로 넣는 번호, 그리고 그 좌표가 들어간다.
+            bo[row][col] = i  # 1-1. 만약 valid하다고 판단되었다면, 위와 같이 보드에 추가해준다.
             if solve(bo):
+                # 그 상태에서 재귀적으로 solve함수를 호출하는데,
+                # 만약 True를 반환한다면,
+                # 즉 더이상 0이 없다면 함수는 여기서 끝나고 그렇지 않은 경우 다시 재귀적으로 0이 없어질때까지 호출될 것이다.
                 return True
+
+
+            # 1-2. 그리고, valid하다고 판단되지 않은 경우 False를 반환
             bo[row][col] = 0
-    # valid하다고 판단되지 않은 경우 False를 반환
     return False
+
 def vaild(bo, num, pos):
+    # valid한 num인지 해당 position(pos)에서 확인하는 함수이다.
+    # 행, 열, 3*3 으로 검사한다.
+
     # check row
-    for i in range(len(bo[0])):
+    for i in range(len(bo[0])): # 0-8을 돌면서,
         if bo[pos[0]][i] == num and pos[1] != i:
+            # num과 보드에서 우리가 확인해야할 좌표의 열에 해당하는 행의 0-8까지 있는 곳의 번호가 같고
+            # 확인해야할 좌표의 행에 해당하는 것이 i가 아니라면(자기 자신은 확인할 필요가 없음)
             return False
+
     # check column
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
+
     # check 3*3 cube
     box_x = pos[1] // 3
     box_y = pos[0] // 3
-    for i in range(box_y*3, box_y*3+3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
+    for i in range(box_y*3, box_y*3+3): # 열
+        for j in range(box_x * 3, box_x * 3 + 3): # 행
+            if bo[i][j] == num and (i, j) != pos: # 같고, 아니라면,
                 return False
     return True # everythings fine
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+
+# 시작점을 찾는다.
+def find_empty(bo): # 보드가 들어간다.
+    for i in range(len(bo)): # 보드의 길이 9만큼,
+        for j in range(len(bo[0])): # 보드 행의 길이 9만큼,
+            if bo[i][j] == 0: # 0이 있는 곳의 좌표를 전달한다.
                 # print(i, j)
                 return (i, j) # row, col
     return None
-def check_board(arr):
-    cnt = 0
-    for y in range(9):
-        for x in range(9):
-            if arr[y][x] == 0:
-                cnt += 1
-    if cnt == (9*9):
-        arr = [[0, 3, 5, 4, 6, 9, 2, 7, 8],[7, 8, 2, 1, 0, 5, 6, 0, 9],[0, 6, 0, 2, 7, 8, 1, 3, 5],[3, 2, 1, 0, 4, 6, 8, 9, 7],[8, 0, 4, 9, 1, 3, 5, 0, 6],[5, 9, 6, 8, 2, 0, 4, 1, 3],[9, 1, 7, 6, 5, 2, 0, 8, 0],[6, 0, 3, 7, 0, 1, 9, 5, 2],[2, 5, 8, 3, 9, 4, 7, 6, 0]]
-        return arr
-    else:
-        return arr
-# print_board(board)
-board = check_board(board)
-res = solve(board)
+
+res = solve(board) # solve라는 main 함수에 넣는다.
 # print(res)
 while res == True:
     board = [i[:] for i in board]
