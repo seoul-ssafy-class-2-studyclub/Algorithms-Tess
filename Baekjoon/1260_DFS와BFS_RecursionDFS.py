7a9e5e8fbf914cb50c92833abded851ed1dbf3b5
tesschung <geobera0910@naver.com>
2019-09-12 21:18:14 +0900

    1260_DFS와BFS_RecursionDFS

--- Baekjoon/1260_DFS와BFS_RecursionDFS.py
+++ Baekjoon/1260_DFS와BFS_RecursionDFS.py
@@ -4,24 +4,28 @@ sys.stdin = open('1260.txt', 'r')
 # parents 가 되는 부분을 출력해야하는데..
 import collections
 
-def DFS(s):
+# 재귀는 for문으로 앞에서 부터 빼오므로 adj_list의 children을 reversed할 필요 없음
+
+def DFS(s, path):
     global adj_list
+    global DFS_res
+    global visited2
     # 재귀스택으로 해보기
     # 찾으면, 작은 숫자부터 출력
-    myres = []
-    stack = collections.deque()
-    stack.append(s)
-    visited = [False]*10001
-    while stack:
-        s = stack.pop()
-        if s not in myres:
-            myres.append(s)
-        visited[s] = True
-        children = list(sorted(adj_list[s], reverse=True)) # 작은 아이부터 출력해야하므로 stack에서는 reverse해준다. (뒤에서부터 pop하므로)
-        for i in children:
-            if visited[i] == False:
-                stack.append(i)
-    return myres
+
+    flag = True
+    for i in adj_list[s]:
+        if visited2[i] == False:
+            flag = False
+            # stack.append(i)
+            visited2[i] = True
+            return DFS(i, path + [i])
+            visited2[i] = False
+
+    if flag:
+        DFS_res.append(path)
+        return True
+
 
 # 1 -> 2 3 4
 # 2 -> 4
@@ -54,7 +58,12 @@ for i in range(M):
     parent, child = map(int, input().split())
     adj_list[parent].append(child)
     adj_list[child].append(parent)
-DFS_res = DFS(Start)
+
+DFS_res = []
+visited2 = [False] * 10001
+visited2[Start] = True
+DFS(Start, [Start])
+print(DFS_res)
 BFS_res = BFS(Start)
 
 for i in DFS_res:
