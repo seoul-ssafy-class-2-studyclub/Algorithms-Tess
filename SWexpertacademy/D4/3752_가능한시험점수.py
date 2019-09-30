1b26b7a21fd9fee366e09b7c426cc43e65deb649
tesschung <geobera0910@naver.com>
2019-09-30 13:31:17 +0900

    3752_가능한시험점수_DP

---
+++ SWexpertacademy/D4/3752_가능한시험점수.py
@@ -0,0 +1,106 @@
+import sys
+sys.stdin = open('3752.txt', 'r')
+
+'''
+#1 7
+#2 11
+'''
+
+# def solve(index, res): #0, 0이다.
+#     global result, cnt
+#
+#     if index == N:
+#         if dp[index].get(res) == None:
+#             dp[index][res] = 1
+#             cnt += 1
+#         return
+#
+#     elif dp[index].get(res) != None: # 만약 dp에 존재한다면 삭제
+#         return
+#
+#     solve(index + 1, res + mylist[index])
+#     solve(index + 1, res)
+#     # res += mylist[index]
+#     dp[index][res] = 1 # 과정을 저장해서 가지치기
+#
+# for tc in range(int(input())):
+#     N = int(input())
+#     # dp = dict()
+#     dp = [{} for _ in range(N + 1)]
+#     mylist = list(map(int, input().split()))
+#     # 하나씩 재귀에 보낸다. -N 까지
+#     cnt = 0
+#     solve(0, 0)
+#     print(dp)
+#     print(f'#{tc+1}', cnt)
+
+for case in range(1, int(input()) + 1):
+    N = int(input())
+    scores = list(map(int, input().split()))
+    dp = {0}
+    for score in scores:
+        temp = set()
+        for num in dp: # dp에 이미 저장된 값에 더하면서 temp에 추가하고,
+            temp.add(score + num)
+        # print(temp)
+        dp.update(temp) # set을 연이어 추가하는 것이므로 update한다.
+        # print(dp)
+    print(f'#{case} {len(dp)}')
+
+# 최적화 필요
+# def solve(index, res): #0, 0이다.
+#     global result
+#
+#     if index == N:
+#         return
+#
+#     else:
+#         solve(index + 1, res + mylist[index])
+#         solve(index + 1, res)
+#         res += mylist[index]
+#         # if res in result:
+#         #     return
+#         # else:
+#         result.add(res)
+#
+# for tc in range(int(input())):
+#     N = int(input())
+#
+#     result = set()
+#     mylist = list(map(int, input().split()))
+#     # 하나씩 재귀에 보낸다. -N 까지
+#     solve(0, 0)
+#     print(f'#{tc+1}', len(result)+1)
+
+
+'''
+def bubun(res, idx):
+    global cnt
+​
+    if idx == N:
+        if maked.get(res) == None:
+            cnt += 1
+            maked[res] = 1
+        return
+​
+    if cache[idx].get(res) != None:
+        return
+    
+    bubun(res + scores[idx], idx + 1)
+    bubun(res , idx + 1)
+​
+    cache[idx][res] = 1
+    return 
+​
+for ro in range(int(input())):
+    N = int(input())
+    scores = list(map(int,input().split()))
+​
+    maked = dict()
+    cnt = 0
+    cache = [{} for _ in range(N)]
+    
+    bubun(0, 0)
+    print('#%d %d' %(ro + 1, cnt))
+
+'''
---
+++ SWexpertacademy/D5/1247_최적경로_순열.py
@@ -0,0 +1,72 @@
+import sys
+sys.stdin = open('1247.txt', 'r')
+
+'''
+#1 200
+#2 304
+#3 366
+
+회사와 집의 위치, 각 고객의 위치
+이차원 정수 좌표 (x, y)로 주어지고 (0 ≤ x ≤ 100, 0 ≤ y ≤ 100)
+
+두 위치 (x1, y1)와 (x2, y2) 사이의 거리는 |x1-x2| + |y1-y2|으로 계산
+
+|x|는 x의 절대값을 의미하며 |3| = |-3| = 3이다. 
+회사의 좌표, 집의 좌표, 고객들의 좌표는 모두 다르다.
+
+회사에서 출발하여 N명의 고객을 모두 방문하고 집으로 돌아오는 경로 중 가장 짧은 것을 찾으려 한다.
+
+
+
+회사와 집의 좌표가 주어지고, 2명에서 10명 사이의 고객 좌표가 주어질 때,
+
+회사에서 출발해서 이들을 모두 방문하고 집에 돌아가는 경로 중
+총 이동거리가 가장 짧은 경로를 찾는 프로그램을 작성
+
+
+각 테스트 케이스의 첫째 줄에는 고객의 수 N이 주어진다. 
+둘째 줄에는 회사의 좌표, 집의 좌표, N명의 고객의 좌표가 차례로 나열된다.
+좌표는 (x,y)쌍으로 구성되는데 입력에서는 x와 y가 공백으로 구분되어 제공
+'''
+# import itertools
+#
+#
+# def solve(xys):
+#     global mymin
+#
+#     # 모든 xys를 계산식에 따라 돌면서 mymin을 갱신하되,
+#     # 갱신된 mymin보다 클때 가지치기 해주면된다.
+#
+#     result = 0
+#     for i in range(0, N+2-1):
+#         result += abs(xys[i][0] - xys[i+1][0]) + abs(xys[i][1] - xys[i+1][1])
+#
+#     if result < mymin:
+#         mymin = result
+#     return
+#
+# for tc in range(int(input())):
+#     N = int(input())
+#     # 받고 나눠주기
+#     mytemp = list(map(int, input().split()))
+#     companyx, companyy = mytemp[0], mytemp[1]
+#     housex, housey = mytemp[2], mytemp[3]
+#     customers = mytemp[4:]
+#
+#     all_customersxy = []
+#     for idx in range(N*2):
+#         if idx%2 == 0:
+#             all_customersxy.append((customers[idx], customers[idx+1]))
+#
+#     candidates = set(itertools.permutations(all_customersxy, N))
+#
+#     # 뽑은 모든 경우의 수를 하나씩 for문으로 순회하면서 이동거리가 가장 짧은 경로를 찾는다.
+#     # set에 값을 넣으면서 개수구하기
+#     mymin = 99999
+#     for candidate in candidates:
+#         candidate = list(candidate)
+#         candidate.insert(0, (companyx, companyy))
+#         candidate.append((housex, housey))
+#         solve(candidate)
+#
+#     print(f'#{tc+1}', mymin)
