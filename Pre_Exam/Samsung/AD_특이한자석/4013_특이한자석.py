47c08778a1586e2b8d50044aa5efeb93b586cd4b
tesschung <geobera0910@naver.com>
2019-11-17 13:58:59 +0900

    1117

--- Pre_Exam/Samsung/AD_특이한자석/4013_특이한자석.py
+++ Pre_Exam/Samsung/AD_특이한자석/4013_특이한자석.py
@@ -59,47 +59,47 @@ for tc in range(1, T+1):
     print(f'#{tc} {res}')
 
 
-
-
-
-
-import collections
-newdir = {1:-1, -1:1}
-myneighbour = {1:[(2, 6, 2)], 2:[(1, 2, 6),(3, 6, 2)], 3:[(2, 2, 6), (4, 6, 2)], 4:[(3,2,6)]}
-score = [0, 1, 2, 4, 8]
-T = int(input())
-for tc in range(1, T+1):
-    K = int(input())
-    magnet = {}
-    for name in range(1, 5):
-        magnet[name] = collections.deque(list(map(int, input().split())))
-    q_info = collections.deque([])
-    for _ in range(K):
-        q_info.append(list(map(int, input().split())))
-    while q_info:
-        name, dir = q_info.popleft()
-        status = [0] * 5
-        vis = [False] * 5
-        q = collections.deque([])
-        q.append((name, dir))
-        vis[name] = True
-        status[name] = dir
-        while q:
-            name, dir = q.popleft()
-            for neighbour, nextidx, myidx in myneighbour[name]:
-                if magnet[neighbour][nextidx] != magnet[name][myidx] and vis[neighbour] == False:
-                    vis[neighbour] = True
-                    status[neighbour] = newdir[dir]
-                    q.append((neighbour, newdir[dir]))
-        for i in range(1, 5):
-            if status[i] == 1:
-                x = magnet[i].pop()
-                magnet[i].appendleft(x)
-            elif status[i] == -1:
-                x = magnet[i].popleft()
-                magnet[i].append(x)
-    res = 0
-    for i in range(1, 5):
-        if magnet[i][0] == 1:
-            res += score[i]
-    print(f'#{tc} {res}')
+#
+#
+#
+#
+# import collections
+# newdir = {1:-1, -1:1}
+# myneighbour = {1:[(2, 6, 2)], 2:[(1, 2, 6),(3, 6, 2)], 3:[(2, 2, 6), (4, 6, 2)], 4:[(3,2,6)]}
+# score = [0, 1, 2, 4, 8]
+# T = int(input())
+# for tc in range(1, T+1):
+#     K = int(input())
+#     magnet = {}
+#     for name in range(1, 5):
+#         magnet[name] = collections.deque(list(map(int, input().split())))
+#     q_info = collections.deque([])
+#     for _ in range(K):
+#         q_info.append(list(map(int, input().split())))
+#     while q_info:
+#         name, dir = q_info.popleft()
+#         status = [0] * 5
+#         vis = [False] * 5
+#         q = collections.deque([])
+#         q.append((name, dir))
+#         vis[name] = True
+#         status[name] = dir
+#         while q:
+#             name, dir = q.popleft()
+#             for neighbour, nextidx, myidx in myneighbour[name]:
+#                 if magnet[neighbour][nextidx] != magnet[name][myidx] and vis[neighbour] == False:
+#                     vis[neighbour] = True
+#                     status[neighbour] = newdir[dir]
+#                     q.append((neighbour, newdir[dir]))
+#         for i in range(1, 5):
+#             if status[i] == 1:
+#                 x = magnet[i].pop()
+#                 magnet[i].appendleft(x)
+#             elif status[i] == -1:
+#                 x = magnet[i].popleft()
+#                 magnet[i].append(x)
+#     res = 0
+#     for i in range(1, 5):
+#         if magnet[i][0] == 1:
+#             res += score[i]
+#     print(f'#{tc} {res}')
