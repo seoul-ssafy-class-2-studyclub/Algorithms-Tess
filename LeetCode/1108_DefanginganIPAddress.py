5ba9c379297234d94364db448e4a7066847c4b5d
tesschung <geobera0910@naver.com>
2019-12-04 10:29:30 +0900

    1108

---
+++ LeetCode/1108_DefanginganIPAddress.py
@@ -0,0 +1,21 @@
+'''
+simple problem
+used the for loop for iterating over the list of the str elements
+I used the fin for as a point to differ '[.' or ']' when it comes to '.'
+'''
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        # print(address)
+        ans = ''
+        fin = 0
+        for char in address:
+            if char == '.':
+                if fin == 0:
+                    ans += '[.'
+                    fin = 1
+                if fin == 1:
+                    ans += ']'
+                    fin = 0
+            if char != '.':
+                ans += char
+        return ans
