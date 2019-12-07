dfce8ba2f3a43209565bc9c2391cadf8f79c7378
tesschung <geobera0910@naver.com>
2019-12-07 19:51:33 +0900

    17836

---
+++ Baekjoon/17836_공주님을구해라!.py
@@ -0,0 +1,62 @@
+import sys
+# sys.stdin = open('17836.txt', 'r')
+
+input = sys.stdin.readline
+import collections
+
+def solve():
+    global maze, T
+    q = collections.deque([])
+    q.append((SY, SX, 0))
+    maze[SY][SX] = 1
+    ans = 1e9
+    comp = 1e9
+    goal = 0
+
+    while q:
+
+        for _ in range(len(q)):
+            sy, sx, cnt = q.popleft()
+
+            if sy == EY and sx == EX:
+                if ans > cnt:
+                    ans = cnt
+                    goal = 1
+
+            if T + 1 < cnt:
+                if goal == 0:
+                    return goal, comp, ans
+
+            for dy, dx in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                iy = dy + sy
+                ix = dx + sx
+                if 0 <= iy < N and 0 <= ix < M:
+                    if maze[iy][ix] == 2:  # 2면, 뚫고 갈 수 있는 시간중 가장 짧은 시간을 바로 찾을 수 있다.
+                        maze[iy][ix] = 1
+
+                        # 정리
+                        if comp == 1e9:  # 1을 뚫고가는 가장 빠른길
+                            comp = abs(iy - EY) + abs(ix - EX) + cnt + 1
+                            goal = 1
+
+                        q.append((iy, ix, cnt + 1))  # 2를 얻고 0만 갈 수 있기 때문에
+
+                    if maze[iy][ix] == 0:  # 0인 길만 갈 수 있다.
+                        maze[iy][ix] = 1
+                        q.append((iy, ix, cnt + 1))
+        T -= 1
+    return goal, comp, ans
+
+# y, x, 제한시간
+N, M, T = map(int, input().split())
+maze = [ list(map(int, input().split())) for _ in range(N) ]
+EY = N-1
+EX = M-1
+SY = 0
+SX = 0
+goal, comp, ans = solve()
+
+if goal:
+    print(min(comp, ans))
+else:
+    print('Fail')
