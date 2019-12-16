18af157cb5c28dec613e797eafc3131f050fe801
tesschung <geobera0910@naver.com>
2019-12-17 08:52:59 +0900

    1217

---
+++ Baekjoon/1613_역사_TopologicalSorting.py
@@ -0,0 +1,100 @@
+import sys
+sys.stdin = open('1613.txt', 'r')
+#
+# from pprint import pprint
+# '''
+# 0
+# -1
+# 1
+# '''
+#
+# n, k = map(int, input().split())
+#
+# adj_arr = [[0]*(n+1) for _ in range(n+1)]
+# indegree = [0]*(n+1)
+# recognition = [False]*(n+1)
+# for _ in range(k):
+#     P, C = map(int, input().split())
+#     adj_arr[P][C] = -1
+#     adj_arr[C][P] = 1
+#     indegree[C] += 1
+#     if not recognition[P]:
+#         recognition[P] = True
+#     if not recognition[C]:
+#         recognition[C] = True
+#
+# # print(adj_arr)
+# # print(indegree)
+# q = []
+# for idx in range(1, n+1):
+#     if indegree[idx] == 0 and recognition[idx] == True:
+#         q.append(idx)
+#
+# res = []
+# while q:
+#
+#     for _ in range(len(q)):
+#         p = q.pop(0)
+#         res.append(p)
+#         for idx in range(1, len(adj_arr[p])):
+#             if adj_arr[p][idx] == -1:
+#                 indegree[idx] -= 1
+#                 if indegree[idx] == 0:
+#                     q.append(idx)
+# # pprint(adj_arr)
+# # print(indegree)
+# # print(res)
+#
+# # for name in res:
+# #     for idx in range(1, n+1):
+# #         if recognition[idx] == True:
+# #             if adj_arr[name][idx] == 0:
+# #                 adj_arr[name][idx] = -1
+# #                 adj_arr[idx][name] = 1
+# # pprint(adj_arr)
+# ans = []
+# tc = int(input())
+# for _ in range(tc):
+#     F, B = map(int, input().split())
+#     ans.append(adj_arr[F][B])
+# print('\n'.join(map(str,ans)))
+
+
+
+'''
+플로이드 와샬 알고리즘은 단 4줄이기 때문에 외우기 굉장히 쉽다.
+for k for i for j ,  ij, ikj 이렇게만 외우면 끝
+플로이드를 돌린 후
+서로 다른 두 사건의 번호가 주어질 때(만약 a,b라고 주어질 때)
+D[a][b] != INF 이면 -1
+D[b][a] != INF 이면 1
+D[a][b] == INF 이면 0을 출력하면 된다.
+'''
+import sys
+input = sys.stdin.readline
+n,m=map(int,input().split())
+
+Max = 1e9
+S = [[Max]*(n+1) for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    S[a][b] = 1
+
+for k in range(n+1): # 거쳐 가는 곳
+    for i in range(n+1): # 시작
+        for j in range(n+1): # 도착
+            S[i][j] = min(S[i][j], S[k][j]+S[i][k])
+res = []
+for i in range(int(input())):
+    a,b = map(int,input().split())
+    if S[a][b] != Max:
+        res += ['-1']
+        continue
+    elif S[b][a] != Max:
+        res += ['1']
+        continue
+    else:
+        res += ['0']
+print('\n'.join(res))
+
+
--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,15 +1,128 @@
 # https://wayhome25.github.io/cs/2017/04/03/cs-03/
 
-from functools import reduce
+# from functools import reduce
+#
+# # 문제1. 전통적으로 최대값 구하기
+# def maximum(li):
+#     default = 0
+#     for e in li:
+#         if default < e:
+#             default = e
+#     return default
+# maximum(li)
+#
+# # 문제1. reduce 활용하여 최대값 구하기
+# reduce(lambda a,b: a if a > b else b ,li)
 
-# 문제1. 전통적으로 최대값 구하기
-def maximum(li):
-    default = 0
-    for e in li:
-        if default < e:
-            default = e
-    return default
-maximum(li)
 
-# 문제1. reduce 활용하여 최대값 구하기
-reduce(lambda a,b: a if a > b else b ,li)
+
+# def next_permutation(s):
+#   for i in reversed(range(len(s))):
+#     if s[i] > s[i-1]:
+#       break
+#   else:
+#     return []
+#   i -= 1
+#   for j in reversed(range(i + 1, len(s))):
+#     if s[j] > s[i]: break
+#   t = s[i]
+#   s[i] = s[j]
+#   s[j] = t
+#   s[i + 1:] = reversed(s[i + 1:])
+#   return s
+#
+# print(next_permutation([1,2,3]))
+
+# the largest index where A[i] < A[i + 1]
+# -1 if there is no such i where A[i] < A[i + 1]
+
+
+#
+# def get_split_index(A):
+#     i = len(A) - 1
+#     while i > 0:
+#         if A[i] < A[i - 1]:
+#             i -= 1
+#         else:
+#             break
+#     return i - 1
+#
+#
+# def get_change_index(A, i):
+#     j = len(A) - 1
+#     while j >= i:
+#         if A[j] > A[i]:
+#             break
+#         else:
+#             j -= 1
+#     return j
+#
+#
+# def swap(A, i, j):
+#     A[i], A[j] = (A[j], A[i])
+#
+#
+# # reverse A[start:]
+# def reverse(A, start):
+#     left = start
+#     right = len(A) - 1
+#     while left < right:
+#         swap(A, left, right)
+#         left += 1
+#         right -= 1
+#
+#
+# def next_permutation(A):
+#     split_index = get_split_index(A)
+#     # the Array is sorted in descreased order
+#     if split_index == -1:
+#         reverse(A, 0)
+#     else:
+#         change_index = get_change_index(A, split_index)
+#         swap(A, split_index, change_index)
+#         reverse(A, split_index + 1)
+#
+#
+#
+#
+# for i in range(30):
+#     next_permutation(A)
+#     print(A)
+
+A = [1, 2, 3, 4]
+
+def next_permutation(s):
+
+    i = len(s) - 1
+    while i > 0:
+        if s[i] < s[i -1]:
+            i -= 1
+        else:
+            break
+    i -= 1
+
+    if i == -1:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+    else:
+        j = len(s) - 1
+        while j >= 1:
+            if s[j] > s[i]:
+                break
+            else:
+                j-=1
+        s[i], s[j] = s[j], s[i]
+        left = i+1
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+for _ in range(23):
+    next_permutation(A) # [1, 3, 2]
+    print(A)
