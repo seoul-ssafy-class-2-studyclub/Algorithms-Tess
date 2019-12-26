96bff906d760dafb12e5c5f2aa5698a2ea993482
tesschung <geobera0910@naver.com>
2019-12-27 08:34:51 +0900

    1227

---
+++ Baekjoon/Java/helloworld.java
@@ -0,0 +1,13 @@
+public class helloworld { // 가장 메인이 되는 class는 파일명과 동일
+    // 메인 메소드를 포함하는 클래스: 메인클래스
+    public static void main(String[] args) { // 반드시 있어야 실행가능
+    // 메인함수
+        System.out.println("Hello World!");
+    }
+}
+
+// 백준에서 시작하려면 Main
+// 소프트웨어아카데미에서는 Solution
+
+
+
