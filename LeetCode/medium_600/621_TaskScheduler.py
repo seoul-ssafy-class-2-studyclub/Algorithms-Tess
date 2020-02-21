b207bc15f5af1d97726ba7901d6fa8c40819c683
tesschung <geobera0910@naver.com>
2020-02-21 14:27:04 +0900

    621_TaskScheduler.py

--- LeetCode/medium_600/621_TaskScheduler.py
+++ LeetCode/medium_600/621_TaskScheduler.py
@@ -1,14 +1,35 @@
+
+
 import collections
 def leastInterval(tasks, n):
-
-    print(tasks, n)
     # Count
     counts = collections.Counter(tasks)
-    print(counts)
+    sortedList = []
+    for key, val in counts.items():
+        sortedList.append([val, key])
+    sortedList.sort(reverse=True)
+    print(sortedList) 
+    ans = []
+    cnt, key = sortedList.pop(0)
+    for _ in range(cnt):
+        cnt -= 1
+        ans.append(key)
+        for _ in range(n):
+            ans.append(False)
+    while sortedList:
+        cnt, key = sortedList.pop(0)
+        for i in range(len(ans)):
+            if ans[i] == False:
+                print(ans[i])
+                cnt -= 1
+                ans[i] = key
+                
+            else:
+                continue
+    print(ans)
 
 
-
-tasks = ["A", "A", "A", "B", "B", "B"]
+tasks = ["A", "A", "A", "A", "B", "B", "B"]
 n = 2
 print(leastInterval(tasks, n))
 
