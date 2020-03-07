48e13645de7b735c12493ece95f3e8923d926290
tesschung <geobera0910@naver.com>
2020-03-08 00:14:37 +0900

    Java TwoSum

--- LeetCode/Java/src/TwoSum.java
+++ LeetCode/Java/src/TwoSum.java
@@ -5,19 +5,10 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> complement = new HashMap<>();
         for (int i=0; i < nums.length; i++ ) {
-            System.out.println(complement);
-//            System.out.println();
-            System.out.println(complement.containsKey(nums[i]));
             if (complement.containsKey(nums[i]) == false) {
-                System.out.println(nums[i]);
                 complement.put(target-nums[i], i);
             } else { // true 라면,
-                int[] ans = new int[2];
-                System.out.println(ans.toString());
-                ans[0] = complement.get(nums[i]);
-                ans[1] = i;
-                System.out.println(ans);
-                return ans;
+                return new int[] {complement.get(nums[i]), i};
             }
         }
         // 자바는 꼭 리턴값이 있어야 한다.
